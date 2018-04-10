70a994766a880f61bc137e4c1f9e7b0697d7d035
Andrew Aitken-Fincham <andrew@silverstripe.com>
2018-04-10 19:54:05 +0100

    implement simple exec method and basic test pass

--- src/Processor.php
+++ src/Processor.php
@@ -4,12 +4,54 @@ namespace AndrewAndante\DIFizzBuzz;
 
 class Processor
 {
-    public static function exec($end, $start)
+    /**
+     * Primary execution class.
+     *
+     * @param int   $start  First number to process
+     * @param int   $end    Last number to process
+     * @param bool  $log    Whether or not to log the output
+     *
+     * @return string[]
+     */
+    public static function exec(int $start, int $end, bool $log = false) : array
     {
-        // noop
+        $output = [];
+        $formatter = new \NumberFormatter("en", \NumberFormatter::SPELLOUT);
+        for ($i = $start; $i <= $end; ++$i)
+        {
+            $string = $formatter->format($i);
+            $string .= ($i % 3 == 0) ? " fizz" : "";
+            $string .= ($i % 5 == 0) ? " buzz" : "";
+
+            if ($log) {
+                self::log($string);
+            }
+
+            $output[] = $string;
+        }
+
+        return $output;
     }
 
-    protected static function log($message)
+    /**
+     * Helper method for most common use-case (start at 1)
+     *
+     * @param int   $end    Last number to process
+     * @param bool  $log    Whether or not to log the output
+     *
+     * @return string[]
+     */
+    public static function execTo(int $end, bool $log = false) : array
+    {
+        return self::exec(1, $end, $log);
+    }
+
+    /**
+     * Helper method to log appropriately for viewing method
+     *
+     * @param string $message
+     */
+    protected static function log(string $message)
     {
         if (PHP_SAPI == 'cli') {
             echo $message . PHP_EOL;
--- tests/php/ProcessorTest.php
+++ tests/php/ProcessorTest.php
@@ -8,22 +8,23 @@ class ProcessorTest extends \PHPUnit_Framework_TestCase
 {
     public function testExec()
     {
-        $expected = "one
-        two
-        three fizz
-        four
-        five buzz
-        six fizz
-        seven
-        eight
-        nine fizz
-        ten buzz
-        eleven
-        twelve fizz
-        thirteen
-        forteen
-        fifteen fizz buzz
-        ";
+        $expected = [
+            'one',
+            'two',
+            'three fizz',
+            'four',
+            'five buzz',
+            'six fizz',
+            'seven',
+            'eight',
+            'nine fizz',
+            'ten buzz',
+            'eleven',
+            'twelve fizz',
+            'thirteen',
+            'fourteen',
+            'fifteen fizz buzz',
+        ];
 
         $this->assertEquals($expected, Processor::exec(1, 15));
     }
